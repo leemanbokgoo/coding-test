12eda48ca3b25eb45617dd62e950c13beae06588
leemanbokgoo <dmsql5303@gmail.com>
2025-05-15 21:42:47 +0900

    feat : [문제] 문자열 찾기

---
+++ FindString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FindString {
+
+	public int solution(String str, char t){
+		int answer = 0;
+		// 전부 대문자로 변경
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);
+		// str을 array로 만들어서 향상된 for문 사용.
+		for( char x : str.toCharArray()){
+			if( x == t ) answer++;
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		FindString T = new FindString();
+		Scanner kb = new Scanner(System.in);
+		// 스캐너로 입력한 str 읽기
+		String str = kb.next();
+		// 문자열을 읽음.
+		char c = kb.next().charAt(0);
+
+		System.out.println(T.solution(str, c));
+	}
+}
