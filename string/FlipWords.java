3eda31f720ff6ce4cdb82858ea6bc27b8fc1bac8
leemanbokgoo <dmsql5303@gmail.com>
2025-05-17 16:04:09 +0900

    feat : [문자열] 단어 뒤집기

--- string/FlipWords.java
+++ string/FlipWords.java
@@ -41,7 +41,7 @@ public class FlipWords {
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
 		String[] str = new String[n];
-		for ( int i =0l i <n; i++){
+		for ( int i =0; i <n; i++){
 			str[i] = kb.next();
 		}
 		for(String x : T.solution(n, str)){
