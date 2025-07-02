425299f1f17e33e5a78069d5c8986b7dfe6e8f52
leemanbokgoo <dmsql5303@gmail.com>
2025-07-02 23:29:33 +0900

    feat : [stack] 올바른 괄호

--- stack/CorrectParentheses.java
+++ stack/CorrectParentheses.java
@@ -30,7 +30,7 @@ public class CorrectParentheses {
 	}
 
 	public static void main(String[] args){
-		LargestNumber T = new LargestNumber();
+		CorrectParentheses T = new CorrectParentheses();
 		Scanner kb = new Scanner(System.in);
 		String str = kb.next();
 		System.out.println(T.solution(str));
